594dd2554b5d560574d4c775dab422bfd8e7b209
PeregrinAl <parshintseva-s@mail.ru>
2022-06-13 23:25:52 +0500

    Documentation

--- snake/html/search/all_a.js
+++ snake/html/search/all_a.js
@@ -5,5 +5,6 @@ var searchData=
   ['snake_2',['Snake',['../class_snake.html',1,'Snake'],['../class_snake.html#aa9cbcdb4b25d84cbf83509039cac8d01',1,'Snake::Snake()'],['../class_snake.html#ae5385a19f98145d3a5ae9c0c8d3367f4',1,'Snake::Snake(const int length)']]],
   ['snake_3',['snake',['../_source_8cpp.html#a019a76a0650bba7ccf3ce191f6553f23',1,'Source.cpp']]],
   ['snake_2eh_4',['Snake.h',['../_snake_8h.html',1,'']]],
-  ['source_2ecpp_5',['Source.cpp',['../_source_8cpp.html',1,'']]]
+  ['source_2ecpp_5',['Source.cpp',['../_source_8cpp.html',1,'']]],
+  ['staticdrawservice_2eh_6',['StaticDrawService.h',['../_static_draw_service_8h.html',1,'']]]
 ];
--- snake/html/search/files_2.js
+++ snake/html/search/files_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['snake_2eh_0',['Snake.h',['../_snake_8h.html',1,'']]],
-  ['source_2ecpp_1',['Source.cpp',['../_source_8cpp.html',1,'']]]
+  ['source_2ecpp_1',['Source.cpp',['../_source_8cpp.html',1,'']]],
+  ['staticdrawservice_2eh_2',['StaticDrawService.h',['../_static_draw_service_8h.html',1,'']]]
 ];
